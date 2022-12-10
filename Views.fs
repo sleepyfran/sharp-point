67b8604aad22fcc0f946a4594a3d4257b0b5d5b8
sleepyfran <fgzv@outlook.com>
2022-12-10 18:49:44 +0100

    :sparkles: Mocked slides + Keyboard navigation

--- Views.fs
+++ Views.fs
@@ -6,14 +6,70 @@ open Avalonia.Controls.ApplicationLifetimes
 open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
 open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Types
+open Avalonia.Input
+open Avalonia.Layout
+open Avalonia.Media
 open Avalonia.Themes.Fluent
 open SharpPoint.Domain
 
+(* --- Global state --- *)
+type private CurrentSlide =
+    | Initial
+    | Slide of index: int
+
+type private GlobalState = { CurrentSlide: IWritable<CurrentSlide> }
+let private state = { CurrentSlide = new State<CurrentSlide>(Initial) }
+
 (* --- Views --- *)
+let private initialSlide deck =
+    Component.create (
+        "initial-slide",
+        fun _ ->
+            StackPanel.create [
+                StackPanel.horizontalAlignment HorizontalAlignment.Center
+                StackPanel.verticalAlignment VerticalAlignment.Center
+                StackPanel.children [
+                    TextBlock.create [
+                        TextBlock.fontSize 72
+                        TextBlock.fontWeight FontWeight.Bold
+                        TextBlock.text deck.Title
+                    ]
+                ]
+            ]
+    ) :> IView
+
+let private slide (index: int) slide =
+    Component.create(
+        $"slide-{index}",
+        fun _ ->
+            TextBlock.create [
+                TextBlock.text $"Slide number {index}"
+            ]
+    ) :> IView
+
 let root deck =
-    Component(fun ctx -> TextBlock.create [ TextBlock.text deck.Title ])
+    Component(fun ctx ->
+        let currentSlide = ctx.usePassedRead state.CurrentSlide
+        
+        StackPanel.create [
+            StackPanel.children [
+                match currentSlide.Current with
+                | Initial -> initialSlide deck
+                | Slide idx ->
+                    deck.Slides
+                    |> List.item idx
+                    |> slide idx
+            ]
+        ]
+    )
 
 (* --- Entrypoint --- *)
+let hasSlideAvailableIn idx slides =
+    slides
+    |> List.tryItem idx
+    |> Option.isSome
+
 type MainWindow(deck: Deck) as this =
     inherit HostWindow()
 
@@ -24,6 +80,28 @@ type MainWindow(deck: Deck) as this =
         this.Padding <- Thickness(10, 30, 10, 0)
         this.Content <- root deck
         this.ExtendClientAreaToDecorationsHint <- true
+
+#if DEBUG
+        this.AttachDevTools()
+#endif
+
+    override this.OnKeyDown event =
+        let current = state.CurrentSlide.Current
+            
+        match (current, event.Key) with
+        | Slide 0, Key.Left ->
+            (* Go back to the initial slide *)
+            state.CurrentSlide.Set Initial
+        | Initial, Key.Right when List.isEmpty deck.Slides |> not ->
+            (* Go to the first slide (if any) *)
+            Slide 0 |> state.CurrentSlide.Set
+        | Slide index, Key.Left when deck.Slides |> hasSlideAvailableIn (index - 1) ->
+            (* Go to the previous slide (if any) *)
+            index - 1 |> Slide |> state.CurrentSlide.Set
+        | Slide index, Key.Right when deck.Slides |> hasSlideAvailableIn (index + 1) ->
+            (* Go to the next slide (if any) *)
+            index + 1 |> Slide |> state.CurrentSlide.Set
+        | _ -> ()
 
 type App(deck: Deck) =
     inherit Application()
